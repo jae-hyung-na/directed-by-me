54ea0c7fe85f1b743abed8f005f15d47dcabf03e
jae-hyung-na <socialmedia21.info@gmail.com>
2026-03-19 16:32:01 +0900

    Delete update-role.ts

--- update-role.ts
+++
@@ -1,8 +0,0 @@
-import Database from 'better-sqlite3';
-
-const db = new Database('portfolio.db');
-
-const role = "Producer, Poetry Writer, Voiceover Recordist & Editing, Post-Production Specialist";
-const result = db.prepare("UPDATE projects SET role = ? WHERE title = '우리의 계절'").run(role);
-
-console.log(`Updated project '우리의 계절' with role. Changes: ${result.changes}`);
