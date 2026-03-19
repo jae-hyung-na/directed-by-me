3412e957976f854b187b2fd17b7472d78ce448c2
jae-hyung-na <socialmedia21.info@gmail.com>
2026-03-19 16:04:28 +0900

    Delete update-db.ts

--- update-db.ts
+++
@@ -1,5 +0,0 @@
-import Database from "better-sqlite3";
-
-const db = new Database("portfolio.db");
-const result = db.prepare("UPDATE projects SET thumbnail_url = 'YAY.png', video_url = 'https://www.youtube.com/embed/XHVuoRXXWJM' WHERE title LIKE '%ZICO X Crush%'").run();
-console.log("Updated rows:", result.changes);
